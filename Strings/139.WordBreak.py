c4bed918d707a081cad2b1fa569544a98f605d6b
Archana-Vellanki <archana.vellanki95@gmail.com>
2023-03-19 16:08:48 -0700

    139, 237

---
+++ Strings/139.WordBreak.py
@@ -0,0 +1,78 @@
+# 139. Word Break
+# Medium
+
+# Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+
+# Example 1:
+
+# Input: s = "leetcode", wordDict = ["leet","code"]
+# Output: true
+# Explanation: Return true because "leetcode" can be segmented as "leet code".
+# Example 2:
+
+# Input: s = "applepenapple", wordDict = ["apple","pen"]
+# Output: true
+# Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+# Note that you are allowed to reuse a dictionary word.
+# Example 3:
+
+# Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+# Output: false
+
+
+# Constraints:
+
+# 1 <= s.length <= 300
+# 1 <= wordDict.length <= 1000
+# 1 <= wordDict[i].length <= 20
+# s and wordDict[i] consist of only lowercase English letters.
+# All the strings of wordDict are unique.
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        # Time complexity: O(N^2)
+        wordsFound = {}
+        return self.helper(s, wordDict, wordsFound)
+
+    def helper(self, s, wordDict, wordsFound):
+        if s in wordsFound:
+            return wordsFound[s]
+        if not s:
+            return True
+
+        for right in range(1, len(s) + 1):
+            if s[:right] in wordDict and self.helper(s[right:], wordDict, wordsFound):
+                wordsFound[s] = True
+                return True
+        wordsFound[s] = False
+        return False
+
+
+# without memoization the time complexity would be exponential
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        # Time complexity: O(2^N)
+        n = len(s)
+        if n == 0:
+            return True
+        right = 0
+        result = False
+        while right <= n and not result:
+            if s[:right] in wordDict:
+                result = self.wordBreak(s[right: n], wordDict)
+            right += 1
+        return result
