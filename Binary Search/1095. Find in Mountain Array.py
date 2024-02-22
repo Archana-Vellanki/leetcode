ae924572c1edf030266ee2594769e561537559cc
Archana-Vellanki <archana.vellanki95@gmail.com>
2024-02-22 00:05:00 -0800

    136

--- Binary Search/1095. Find in Mountain Array.py
+++ Binary Search/1095. Find in Mountain Array.py
@@ -1,9 +1,6 @@
 # 1095. Find in Mountain Array
-# Solved
 # Hard
-# Topics
-# Companies
-# Hint
+
 # (This problem is an interactive problem.)
 
 # You may recall that an array arr is a mountain array if and only if:
@@ -20,7 +17,6 @@
 # MountainArray.length() returns the length of the array.
 # Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer. Also, any solutions that attempt to circumvent the judge will result in disqualification.
 
- 
 
 # Example 1:
 
@@ -32,7 +28,7 @@
 # Input: array = [0,1,2,4,2,1], target = 3
 # Output: -1
 # Explanation: 3 does not exist in the array, so we return -1.
- 
+
 
 # Constraints:
 
@@ -40,11 +36,12 @@
 # 0 <= target <= 109
 # 0 <= mountain_arr.get(index) <= 109
 
+
 # """
 # This is MountainArray's API interface.
 # You should not implement it, or speculate about its implementation
 # """
-#class MountainArray(object):
+# class MountainArray(object):
 #    def get(self, index):
 #        """
 #        :type index: int
@@ -55,6 +52,14 @@
 #        """
 #        :rtype int
 #        """
+
+
+# Time complexity: O(logn)
+# Space complexity: O(1)
+
+# Approach: find the peak index of mountain array. binary search in the left half first.
+# if target is not found in the left side of the peak, binary search in the right part of the peak.
+
 
 class Solution(object):
     def findPeakIndex(self, arr):
@@ -67,9 +72,9 @@ class Solution(object):
             else:
                 r = mid
         return r
+
     def binarySearch(self, left, right, arr, target, isDesc):
         while left < right:
-            # print("left:", left, "right:", right)
             mid = (left + right)//2
             if arr.get(mid) > target:
                 if isDesc:
@@ -83,10 +88,9 @@ class Solution(object):
                     left = mid + 1
             else:
                 return mid
-        # print(left, right, arr.get(left), arr.get(right), target)
+
         return left if arr.get(left) == target else -1
-            
-    
+
     def findInMountainArray(self, target, mountain_arr):
         """
         :type target: integer
@@ -94,13 +98,81 @@ class Solution(object):
         :rtype: integer
         """
         peak = self.findPeakIndex(mountain_arr)
-        print(peak)
 
         result = self.binarySearch(0, peak, mountain_arr, target, False)
-        print(result)
+
         return result if result != -1 else self.binarySearch(peak, mountain_arr.length()-1, mountain_arr, target, True)
 
 
+# old solution same approach but avg implementation
+class Solution(object):
+    def peakIndexInMountainArray(self, mountain_arr, length):
+        # Corner cases: [1,2,3], [3,2,1], [1,3,2], [2,3,2], [2,3,1], [9,3,2,1], [1,2,3,4]
+        left = 0
+        right = length - 1
 
+        while left < right:
+            mid = (left+right)//2
+            mid_element = mountain_arr.get(mid)
+            post_mid = mountain_arr.get(mid + 1)
 
-        
+            if mid_element < post_mid:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
+
+    def binarySearch(self, left, right, target, mountain_arr, isDesc):
+        """
+        Time Complexity: O(logN)
+        Space Complexity: O(1)
+        isDesc denotes if the array is ascending or descending
+        """
+        while left < right:
+            mid = (left + right)//2
+            mid_element = mountain_arr.get(mid)
+            if mid_element == target:
+                return mid
+            elif mid_element < target:
+                if isDesc:
+                    right = mid
+                else:
+                    left = mid + 1
+            else:
+                if isDesc:
+                    left = mid + 1
+                else:
+                    right = mid
+        if mountain_arr.get(left) == target:
+            return left
+        else:
+            return -1
+
+    def findInMountainArray(self, target, mountain_arr):
+        """
+        :type target: integer
+        :type mountain_arr: MountainArray
+        :rtype: integer
+        """
+        length = mountain_arr.length()
+
+        # find Peak position
+        peak = self.peakIndexInMountainArray(mountain_arr, length)
+        peak_element = mountain_arr.get(peak)
+
+        # check if the target is out of bounds or if it is the peak
+        if target > peak_element:
+            return -1
+        elif target < mountain_arr.get(0) and target < mountain_arr.get(length - 1):
+            return -1
+        elif peak_element == target:
+            return peak
+
+        # search in the ascending part
+        index = self.binarySearch(0, peak, target, mountain_arr, False)
+        if index != -1:
+            return index
+        else:
+            # the target is not present in the ascending part hence it is searched for in the descending part i.e., after the peak
+            return self.binarySearch(peak, length - 1, target, mountain_arr, True)
